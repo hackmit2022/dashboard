163a2922af6ce01715b634a80b396706ef0741ad
Luc Gaitskell <me@lucg.xyz>
2022-10-02 10:28:46 -0400

    Use KG instead

--- C02_per_block.py
+++ C02_per_block.py
@@ -4,7 +4,7 @@ def calculate_CO2_per_block(difficulty):
     max_hashes_per_joule = 11100 * 1000000
     joules_per_block = hashes_per_block / max_hashes_per_joule
     kwh = joules_per_block / 3.6e6
-    pounds_CO2_per_kwh = 0.82
-    CO_2_per_block = kwh * pounds_CO2_per_kwh
+    kg_CO2_per_kwh = 0.82 * 0.45
+    CO_2_per_block = kwh * kg_CO2_per_kwh
 
     return CO_2_per_block
