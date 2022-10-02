273765a3b0d018a812769661f849eb950b7c28cd
Luc Gaitskell <me@lucg.xyz>
2022-10-02 10:50:31 -0400

    Add energy totals + fix calc

--- C02_per_block.py
+++ C02_per_block.py
@@ -1,9 +1,15 @@
-def calculate_CO2_per_block(difficulty):
+def calculate_energy_per_block(difficulty):
 
     hashes_per_block = 4.3e9 * difficulty
     max_hashes_per_joule = 11100 * 1000000
     joules_per_block = hashes_per_block / max_hashes_per_joule
-    kwh = joules_per_block / 3.6e6
+    kwh_per_block = joules_per_block / 3.6e6
+    kwh_per_transaction = kwh_per_block / 1800
+    return kwh_per_transaction
+
+
+def calculate_CO2_per_block(difficulty):
+    kwh = calculate_energy_per_block(difficulty)
     kg_CO2_per_kwh = 0.82 * 0.45
     CO_2_per_block = kwh * kg_CO2_per_kwh
 
--- graph.py
+++ graph.py
@@ -1,6 +1,6 @@
 from dash import Dash, dcc, html, Input, Output, dash_table
 from data import close_diff, Difficulty, WalletInfo
-from C02_per_block import calculate_CO2_per_block
+from C02_per_block import calculate_CO2_per_block, calculate_energy_per_block
 
 app = Dash(__name__)
 
@@ -41,32 +41,38 @@ def update_wallet_display(hash):
     wi = WalletInfo.from_hash(hash)
 
     transaction_data = [
-        (t, diff, calculate_CO2_per_block(diff))
+        (t, diff, calculate_energy_per_block(diff), calculate_CO2_per_block(diff))
         for t, diff in ((t, close_diff(t, diff)) for t in wi.transactions)
     ]
 
     totals = {
-        "fees": sum(t.fee for t, _, _ in transaction_data),
-        "CO2": sum(co2 for _, _, co2 in transaction_data),
+        "fees": sum(t.fee for t, _, _, _ in transaction_data),
+        "CO2": sum(co2 for _, _, _, co2 in transaction_data),
+        "energy": sum(energy for _, _, energy, _ in transaction_data),
     }
 
     return [
-        dash_table.DataTable(
-            data=[
-                {
-                    "date": t.time.strftime("%Y-%m-%d"),
-                    "fee (sat)": t.fee,
-                    "CO2 (kg)": f"{co2:.8}",
-                }
-                for t, diff, co2 in transaction_data
-            ],
-        ),
+        [
+            html.H3("Transactions:"),
+            dash_table.DataTable(
+                data=[
+                    {
+                        "date": t.time.strftime("%Y-%m-%d"),
+                        "fee (sat)": t.fee,
+                        "CO2 (kg)": f"{co2:.8}",
+                        "energy (kWh)": f"{energy:.8}",
+                    }
+                    for t, diff, energy, co2 in transaction_data
+                ],
+            ),
+        ],
         [
             html.H2("Your totals"),
             html.P(
                 f"Total fees: {totals['fees']/1e8} BTC = {totals['fees']/1e8 * 19158.40:.2f} USD"
             ),
             html.P("Total CO2: {:.2f} kg".format(totals["CO2"])),
+            html.P("Total Energy: {:.0f} kWh".format(totals["energy"])),
         ],
     ]
 
